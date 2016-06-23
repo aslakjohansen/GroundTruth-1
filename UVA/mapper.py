8e4f1222d80deafa0a74201009e948df9a21a55d
Dezhi Hong <dzhong1989@gmail.com>
2016-06-23 01:13:25 -0400

    aded to mapper

--- UVA/mapper.py
+++ UVA/mapper.py
@@ -1,8 +1,7 @@
 import rdflib
+import csv
 from rdflib.namespace import OWL, RDF, RDFS
 from rdflib import URIRef
-
-src = [i.strip().split(',')[0] for i in open('Rice_Type').readlines()]
 
 BRICK = rdflib.Namespace('http://buildsys.org/ontologies/Brick#')
 BRICKFRAME = rdflib.Namespace('http://buildsys.org/ontologies/BrickFrame#')
@@ -31,5 +30,19 @@ building_graph.add((RICE.RICE_Floor_2, BRICKFRAME.isPartof, RICE.RICE))
 building_graph.add((RICE.RICE_Floor_3, BRICKFRAME.isPartof, RICE.RICE))
 building_graph.add((RICE.RICE_Floor_4, BRICKFRAME.isPartof, RICE.RICE))
 building_graph.add((RICE.RICE_Floor_5, BRICKFRAME.isPartof, RICE.RICE))
+
+with open('point.csv', 'r') as src:
+    ptList = csv.DictReader(src)
+    for pt in ptList:
+        label = pt['original label']
+        pt_type = pt['parsed tagset']
+        pt_type = BRICK[pt_type]
+        if (pt_type, None, None) in brick_graph:
+            building_graph.add((RICE[label], RDF.type, pt_type))
+        else:
+            building_graph.add((RICE[label], RDF.type, BRICK.Equipment))
+
+        #pt_loc = pt['room']
+        #building_graph.add((pt['original label'], BRICKFRAME.isLocatedIn, RICE[pt_loc]))
 
 building_graph.serialize(destination='test.ttl', format='turtle')
