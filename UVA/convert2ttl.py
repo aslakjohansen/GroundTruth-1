864c9beb73e5a9d05045cb4b8b8f96c757304924
Dezhi Hong <dzhong1989@gmail.com>
2016-06-24 01:32:36 -0400

    edited convert2ttl script

--- UVA/convert2ttl.py
+++ UVA/convert2ttl.py
@@ -36,15 +36,31 @@ with open('point.csv', 'r') as src:
     ptList = csv.DictReader(src)
     for pt in ptList:
         label = pt['original label']
+        pt_type = pt['tagset']
+        pt_ahu = pt['ahu']
+        pt_zone = pt['hvac_zone']
+        pt_floor = pt['floor']
+        pt_room = pt['room']
         label = re.sub('\s+','_',label)
-        pt_type = pt['parsed tagset']
+
+        pt_type = re.sub('\s+','_',pt_type)
         pt_type = BRICK[pt_type]
         if (pt_type, None, None) in brick_graph:
             building_graph.add((RICE[label], RDF.type, pt_type))
-            print pt_type
         else:
+            building_graph.add((RICE[label], RDF.type, OWL.NamedIndividual))
             building_graph.add((RICE[label], RDF.type, BRICK.Point))
-        #pt_loc = pt['room']
-        #building_graph.add((pt['original label'], BRICKFRAME.isLocatedIn, RICE[pt_loc]))
+
+
+        if pt_room:
+            pt_room = BRICK[pt_room]
+            building_graph.add((RICE[label], BRICKFRAME.isLocatedIn, RICE[pt_room]))
+
+            if pt_zone:
+                building_graph.add((RICE["HVAC_Zone" + pt_zone], BRICKFRAME.hasPoint, RICE["Room" + pt_room]))
+                building_graph.add((RICE["Room" + pt_room], BRICKFRAME.isPointOf, RICE["HVAC_Zone" + pt_zone]))
+
+            if pt_floor:
+                building_graph.add((RICE["Floor" + pt_floor], BRICKFRAME.hasPart, RICE["Room" + pt_room]))
 
 building_graph.serialize(destination='Rice.ttl', format='turtle')
