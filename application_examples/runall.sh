348f11176512499383e59990be0516421c038300
Gabe Fierro <gtfierro225@gmail.com>
2016-09-14 09:54:30 -0700

    more generalized runall.sh

--- application_examples/runall.sh
+++ application_examples/runall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for building in ../building_instances/ebu3b_brick.ttl  ../building_instances/ghc_brick.ttl  ../building_instances/gtc_brick.ttl  ../building_instances/rice_brick.ttl  ../building_instances/soda_brick.ttl; do
+for building in `ls ../building_instances/*.ttl`; do
     echo $building
     python RUN_APPS.py $building
 done
