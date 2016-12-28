cfc054cb11b26ef0aa56c53e0f064e1de09435a4
jpsctc <jploennigs@jpmbp.speedport_w_723v_1_41_000>
2016-12-28 20:20:00 +0100

    remove local versions

--- application_examples/runall_jp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for building in ../etc/instance_generators/IBM/IBM_B3.ttl ../etc/instance_generators/SODA_UCB/SODA/berkeley.ttl ../etc/instance_generators/EBU3B_UCSD/ebu3b_brick.ttl ../etc/instance_generators/GTC_SDU/gtc_brick.ttl ../etc/instance_generators/GHC_CMU/GHC_brick.ttl ../etc/instance_generators/RICE_UVA/Rice.ttl; do
-    echo $building
-    python3 RUN_APPS_jp.py $building
-done
